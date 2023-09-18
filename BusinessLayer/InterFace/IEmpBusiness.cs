95d4dc9f04f84a8752ca7151ca9f6a955139f5f7
Upendra Sahu <upendrasahu1199@gmail.com>
2023-09-18 15:34:07 +0530

    [Upendra] Add: Crud_Operation_Logic_In_Business_Layer

---
+++ BusinessLayer/InterFace/IEmpBusiness.cs
@@ -0,0 +1,16 @@
+﻿using CommonLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.InterFace
+{
+    public interface IEmpBusiness
+    {
+        public EmployeeModel AddEmployee(EmployeeModel employeeModel);
+        public IEnumerable<EmployeeModel> GetAllEmployee();
+        public EmployeeModel GetEmployee(int? EmployeeId);
+        public EmployeeModel UpdateEmployee(EmployeeModel employeeModel);
+        public string DeleteEmployee(int? employeeId);
+    }
+}
---
+++ BusinessLayer/Service/EmpBusiness.cs
@@ -0,0 +1,85 @@
+﻿using BusinessLayer.InterFace;
+using CommonLayer.Model;
+using RepoLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Service
+{
+    public class EmpBusiness : IEmpBusiness
+    {
+        private readonly IEmpRepo empRepo;
+
+        public EmpBusiness(IEmpRepo empRepo)
+        {
+            this.empRepo = empRepo;
+        }
+
+        // Add New Employee
+        public EmployeeModel AddEmployee(EmployeeModel employeeModel)
+        {
+            try
+            {
+                return empRepo.AddEmployee(employeeModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Get All Employee
+        public IEnumerable<EmployeeModel> GetAllEmployee()
+        {
+            try
+            {
+                return empRepo.GetAllEmployee();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        //Get Employee By Id
+        public EmployeeModel GetEmployee(int? EmployeeId)
+        {
+            try
+            {
+                return empRepo.GetEmployee(EmployeeId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        // Update Employee
+        public EmployeeModel UpdateEmployee(EmployeeModel employeeModel)
+        {
+            try
+            {
+                return empRepo.UpdateEmployee(employeeModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        //Delete Employee
+        public string DeleteEmployee(int? employeeId)
+        {
+            try
+            {
+                return empRepo.DeleteEmployee(employeeId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
