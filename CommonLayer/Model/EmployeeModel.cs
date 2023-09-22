8bfae170aa63cf910607985514101266e436966d
Upendra Sahu <upendrasahu1199@gmail.com>
2023-09-22 17:17:02 +0530

    [Upendra] Add:Implemented_Form_Validation

--- CommonLayer/Model/EmployeeModel.cs
+++ CommonLayer/Model/EmployeeModel.cs
@@ -8,19 +8,22 @@ namespace CommonLayer.Model
     public class EmployeeModel
     {
         public int EmployeeId { get; set; }
-
+        [Required(ErrorMessage = "{0} Should not be empty")]
+        [RegularExpression(@"[A-Z][a-z]{2,}", ErrorMessage = "Enter Valid Name")]
         public string EmployeeName { get; set; }
-
+        [Required(ErrorMessage = "Choose one")]
         public string ProfileImg { get; set; }
-
+        [Required(ErrorMessage = "Choose one")]
         public string Gender { get; set; }
-   
+        [Required(ErrorMessage = "{0} Should not be empty")]
+        [RegularExpression(@"[A-Z][a-z]{2}", ErrorMessage =  "Enter valid {0}")]
         public string Department { get; set; }
-      
+        [Required(ErrorMessage = "{0} Should not be empty")]
+        [RegularExpression(@"[0-9]+.[0-9]{2}", ErrorMessage = "Enter valid data")]
         public decimal Salary { get; set; }
-        
+        [Required(ErrorMessage = "{0} Should not be empty")]
         public DateTime StartDate { get; set; }
-        
+        [Required(ErrorMessage = "{0} Should not be empty")]
         public string Notes { get; set; }
     }
 }
--- CommonLayer/Model/LoginEmpModel.cs
+++ CommonLayer/Model/LoginEmpModel.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace CommonLayer.Model
 {
     public class LoginEmpModel
     {
+        [Required(ErrorMessage = "{0} Not be Empty")]
+        [Range(1, double.MaxValue, ErrorMessage = "Enter Valid Data")]
         public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "{0} Should not be empty")]
+        [RegularExpression(@"[A-Z][a-z]{2,}", ErrorMessage = "Enter Valid Name")]
         public string EmployeeName { get; set; }
     }
 }
